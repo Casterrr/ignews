dff4e572a690fe1d37b7989b42fff2a2a489f8f5
Lucas <lucasmvl2003@gmail.com>
2022-10-21 12:53:00 -0300

    Testing Components

---
+++ src/components/SubscribeButton/SubscribeButton.spec.tsx
@@ -0,0 +1,84 @@
+import { fireEvent, render, screen } from "@testing-library/react"
+import { signIn, useSession } from 'next-auth/react'
+import { useRouter} from 'next/router'
+import { SubscribeButton } from "."
+
+jest.mock('next-auth/react')
+
+//fn é um função vazia do jest. Semelhante a () => {}
+jest.mock('next/router', () => ({
+    useRouter: jest.fn().mockReturnValue({
+        push: jest.fn()
+    })
+}))
+
+describe('SubscribeButton Component', () => {
+
+    it('renders correctly', () => {
+        const useSessionMocked = jest.mocked(useSession)
+
+        useSessionMocked.mockReturnValueOnce({
+            data: null, 
+            status: 'unauthenticated'
+        })
+
+        render(
+            <SubscribeButton />
+        )
+
+        expect(screen.getByText('Subscribe now')).toBeInTheDocument() 
+    })
+
+    it('redirects user to sign in when not authenticated', () => {
+        const signInMocked = jest.mocked(signIn)
+        const useSessionMocked = jest.mocked(useSession)
+
+        useSessionMocked.mockReturnValueOnce({
+            data: null, 
+            status: 'unauthenticated'
+        })
+
+        render(
+            <SubscribeButton />
+        )
+
+        const subscribleButton = screen.getByText('Subscribe now')
+
+        fireEvent.click(subscribleButton)
+
+        expect(signInMocked).toHaveBeenCalled()
+    })
+
+    it('redirects to posts when user already has a subscription', () => {
+       
+            const useRouterMocked = jest.mocked(useRouter)
+            const useSessionMocked = jest.mocked(useSession)
+            const pushMock = jest.fn()
+
+            useSessionMocked.mockReturnValueOnce({
+                data: {
+                    user: { 
+                        name: "John Doe", 
+                        email: "john.doe@example.com"
+                    },
+                    activeSubscription: 'fake-active-subscription',
+                    expires: "fake-expires"
+                },
+                status: 'authenticated'
+            })
+
+            useRouterMocked.mockReturnValueOnce({
+                push: pushMock
+            } as any)
+    
+            render(
+                <SubscribeButton />
+            )
+    
+            const subscribleButton = screen.getByText('Subscribe now')
+
+            fireEvent.click(subscribleButton)
+
+            expect(pushMock).toHaveBeenCalledWith('/posts')
+    })
+} )
